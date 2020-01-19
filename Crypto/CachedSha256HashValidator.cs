40e20ef71403db3ffab076042fc6965da02b9ddf
Patrik Mjørner <PAM@simcorp.com>
2020-01-19 22:45:26 +0100

    Minor refactoring of CachedAuth

--- Crypto/CachedSha256HashValidator.cs
+++ Crypto/CachedSha256HashValidator.cs
@@ -1,26 +1,23 @@
-using System.Text;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace ReactRedux.Crypto {
     public class CachedSha256HashValidator : Argon2CridentialsValidator {
         private static byte[] ValidatedSha256AuthHeaderBytes = null;
         private const int ShaSaltBytesLength = 16;
-        private static byte[] ShaSaltBytes = null;
+        private static readonly byte[] ShaSaltBytes;
 
         static CachedSha256HashValidator() {
-            using (RandomNumberGenerator rng = RandomNumberGenerator.Create()) {
+            using(RandomNumberGenerator rng = RandomNumberGenerator.Create()) {
                 ShaSaltBytes = new byte[ShaSaltBytesLength];
                 rng.GetBytes(ShaSaltBytes);
             }
         }
 
-        public CachedSha256HashValidator(AppConfiguration configuration) : base(configuration) {}
+        public CachedSha256HashValidator(AppConfiguration configuration) : base(configuration) { }
 
         public override bool Verify(string authHeader) {
-            if (ValidatedSha256AuthHeaderBytes != null) {
-                bool shaVerified = ByteByByteEquals(ComputeSha256Bytes(authHeader), ValidatedSha256AuthHeaderBytes);
-                return shaVerified;
-            }
+            if (VerifyIfCached(authHeader)) { return true; }
 
             bool verified = base.Verify(authHeader);
             if (verified) {
@@ -29,18 +26,24 @@ namespace ReactRedux.Crypto {
             return verified;
         }
 
-        private static bool ByteByByteEquals(byte[] a, byte[] b) {
-            uint diff = (uint)a.Length ^ (uint)b.Length;
+        private bool VerifyIfCached(string authHeader) {
+            if (ValidatedSha256AuthHeaderBytes == null) { return false; }
+            bool verified = SlowByteByByteEquals(ComputeSha256Bytes(authHeader), ValidatedSha256AuthHeaderBytes);
+            return verified;
+        }
+
+        private static bool SlowByteByByteEquals(byte[] a, byte[] b) {
+            uint diff = (uint) a.Length ^ (uint) b.Length;
             for (int i = 0; i < a.Length && i < b.Length; i++) {
-                diff |= (uint)(a[i] ^ b[i]);
+                diff |= (uint) (a[i] ^ b[i]);
             }
             return diff == 0;
         }
 
         private byte[] ComputeSha256Bytes(string input) {
-            using (var sha = SHA256.Create()) {
+            using(var sha = SHA256.Create()) {
                 byte[] bytes = Encoding.UTF8.GetBytes(input);
-                bytes = AddSalt(bytes);
+                bytes = AddSaltBytes(bytes);
                 int iteration = 0;
                 while (iteration++ < base.Configuration.ShaIterations) {
                     bytes = sha.ComputeHash(bytes);
@@ -49,13 +52,13 @@ namespace ReactRedux.Crypto {
             }
         }
 
-        private byte[] AddSalt(byte[] bytes) {
-            byte[] newSaltedBytes = new byte[bytes.Length+ShaSaltBytesLength];
-            for(int i = 0; i < ShaSaltBytesLength; i++) {
+        private byte[] AddSaltBytes(byte[] bytes) {
+            byte[] newSaltedBytes = new byte[bytes.Length + ShaSaltBytesLength];
+            for (int i = 0; i < ShaSaltBytesLength; i++) {
                 newSaltedBytes[i] = ShaSaltBytes[i];
             }
-            for(int i = 0; i < bytes.Length; i++) {
-                newSaltedBytes[ShaSaltBytesLength+i] = bytes[i];
+            for (int i = 0; i < bytes.Length; i++) {
+                newSaltedBytes[ShaSaltBytesLength + i] = bytes[i];
             }
             return newSaltedBytes;
         }
